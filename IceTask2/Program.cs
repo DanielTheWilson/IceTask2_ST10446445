e169b363041ced3da8dceb51cfc92ceb2ea0949d
lab_services_student <lab_services_student@lab7L95SR>
2025-03-04 09:32:45 +0200

    Add project files.

---
+++ IceTask2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace IceTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int choice;
+            WorkerClass worker = new WorkerClass();
+            string sound1 = "1. Wav1";
+            string sound2 = "2. Wav2";
+            string sound3 = "3. Wav3";
+            string end = "4. END";
+
+            do
+            {
+                Console.WriteLine("Pick a sound to play");
+                Console.WriteLine(sound1);
+                Console.WriteLine(sound2);
+                Console.WriteLine(sound3);
+                Console.WriteLine(end);
+              
+             
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch(choice)
+                    {
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Wav 1 is now playing");
+                            Console.ResetColor();
+                            worker.Sound1();
+                            break;
+
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Wav 2 is now playing");
+                            Console.ResetColor();
+                            worker.Sound2();
+                            break;
+                        case 3:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Wav 3 is now playing");
+                            Console.ResetColor();
+                            worker.Sound3();
+                            break;
+                        case 4:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Shutting down program");
+                            Console.ResetColor();
+                            break;
+                    }
+                }
+            }
+            while (choice != 4);
+
+
+        }
+    }
+}
---
+++ IceTask2/WorkerClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace IceTask2
+{
+    internal class WorkerClass
+    {
+       public WorkerClass() { }
+
+        public void Sound1()
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/lab_services_student/Music/WAV sounds/aykroyd_coneheads_unacceptable.wav";
+            player.Play();
+        }
+
+        public void Sound2()
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/lab_services_student/Music/WAV sounds/coburn_payback_who_gives.wav";
+            player.Play();
+        }
+
+        public void Sound3()
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/lab_services_student/Music/WAV sounds/collateral_damage_no.wav";
+            player.Play();
+        }
+
+    }
+}
+
